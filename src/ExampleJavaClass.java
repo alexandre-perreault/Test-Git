d185f6d712e4b90c69a670303edf2c5934b07f56
Alexandre Perreault <perreault.alexandre.3@courrier.uqam.ca>
2023-05-24 09:51:48 -0400

    Add all files

---
+++ src/ExampleJavaClass.java
@@ -0,0 +1,6 @@
+public class ExampleJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    } // fin method main
+
+} // fin class ExampleJavaClass
